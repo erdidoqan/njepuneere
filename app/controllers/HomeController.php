0ca85ee2e2aa66a357428b5deb283e5afd360758
Erdi <erdi.doqan@gmail.com>
2014-10-19 00:48:05 +0200

    home

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -9,7 +9,7 @@ class HomeController extends BaseController {
 	|
 	| You may wish to use controllers instead of, or in addition to, Closure
 	| based routes. That's great! Here is an example controller method to
-	| get you started. To route to this controller, just add the route:
+	| get you started. To rou to this controller, just add the route:
 	|
 	|	Route::get('/', 'HomeController@showWelcome');
 	|
