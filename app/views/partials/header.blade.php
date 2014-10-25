62fe7c91d08220613287053c6c890d767f223f9e
Erdi <erdi.doqan@gmail.com>
2014-10-25 02:35:51 +0200

    unmobilize

--- app/views/partials/header.blade.php
+++ app/views/partials/header.blade.php
@@ -1,11 +1,9 @@
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0">-->
     <meta name="description" content="">
     <meta name="author" content="Erdi Doğan">
-
     <link rel="shortcut icon" href="/ana.ico" type="image/x-icon">
     <link rel="icon" href="/ana.ico" type="image/x-icon">
-
     <title>Njepuneere</title>
 
     <!-- Bootstrap core CSS -->
