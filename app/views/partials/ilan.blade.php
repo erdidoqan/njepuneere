c767e3f332d764d006b9301b048b390e653d0900
Erdi <erdi.doqan@gmail.com>
2014-10-26 01:52:12 +0200

    img width %100 silindi

--- app/views/partials/ilan.blade.php
+++ app/views/partials/ilan.blade.php
@@ -2,12 +2,13 @@
     @foreach($adsInfo as $p)
         <div class="ads">
             <div class="ads-img">
-                <img style="width:100%" src="{{User::$img_sirket_src."".$p->logo}}" />
+                <img src="{{User::$img_sirket_src."".$p->logo}}" />
 
             </div>
 
             <div class="ads-text">
                 <h3> {{$p->com_name}} </h3>
+                <small> {{$p->ref_code}} </small>
             </div>
         </div>
     @endforeach
