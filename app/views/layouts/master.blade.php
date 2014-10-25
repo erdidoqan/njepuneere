a64ce950ff3f9640d451f8b8d3f320f7a0d550a5
Erdi <erdi.doqan@gmail.com>
2014-10-25 02:52:27 +0200

    ilanlar

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -12,7 +12,7 @@
         
             @yield('content')
 <div class="container">
-        <div class="col-sm-8" style="margin-top:30px;">
+        <div class="col-sm-8" style="margin-top:8px;">
            @include('partials/ilan')
          </div>
        <div class="col-sm-4" style="margin-top:30px;">
