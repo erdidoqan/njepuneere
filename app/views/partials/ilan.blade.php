c00f76ac06162a01d1efb5b08ec5d8b5d72b91bc
Erdi <erdi.doqan@gmail.com>
2014-10-26 01:44:39 +0200

    ads duzenle

--- app/views/partials/ilan.blade.php
+++ app/views/partials/ilan.blade.php
@@ -1,24 +1,16 @@
 @if($adsInfo->count())
     @foreach($adsInfo as $p)
- <div class="col-sm-5"  style="max-width:170px; max-height=165px;">
-            <a href="ads/{{$p->id}}">
-                <div style="border-radius:35px;" class="[ info-card ]">
-                    <img style="width:100%" src="{{User::$img_sirket_src."".$p->logo}}" />
-                    <div class="[ info-card-details ] animate">
-                        <div class="[ info-card-header ]">
-                            <h3> {{$p->com_name}} </h3>
-                            <small> {{$p->ref_code}} </small><br>
-                            @if(Auth::user())
-                                <button class="btn btn-primary btn-block btn-xs">Apply</button>
-                            @endif
-                        </div>
-                        
-                    </div>
-                </div> 
-                </a>
-                
+        <div class="ads">
+            <div class="ads-img">
+                <img style="width:100%" src="{{User::$img_sirket_src."".$p->logo}}" />
+
             </div>
-            @endforeach
+
+            <div class="ads-text">
+                <h3> {{$p->com_name}} </h3>
+            </div>
+        </div>
+    @endforeach
 @endif
 
 
