59d658b9f4274d1edcf63a8ab11be8e043093ab9
Erdi <erdi.doqan@gmail.com>
2014-10-26 22:42:27 +0100

    Revert d6e945f..0447eb8
    This rolls back to commit d6e945f2abe601d557a313cae0268b0dfd2a11e7.

--- app/views/partials/ilan.blade.php
+++ app/views/partials/ilan.blade.php
@@ -1,6 +1,5 @@
 @if($adsInfo->count())
     @foreach($adsInfo as $p)
-        <a href="ads/{{$p->id}}">
         <div class="ads">
             <div class="ads-img">
                 <img src="{{User::$img_sirket_src."".$p->logo}}" />
@@ -9,10 +8,9 @@
 
             <div class="ads-text">
                 <p> {{ucwords($p->com_name)}} </p>
-                <small style="margin-bottom:15px;"> {{ucwords($p->ads_name)}} </small>
+                <small> {{$p->ref_code}} </small>
             </div>
         </div>
-        </a>
     @endforeach
 @endif
 
