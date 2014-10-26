bc1f05879e19e00d4c4e1dbdb7d1de07e84b5f0b
Erdi <erdi.doqan@gmail.com>
2014-10-26 23:54:02 +0100

    ilanlara link eklendi

--- app/views/partials/ilan.blade.php
+++ app/views/partials/ilan.blade.php
@@ -1,21 +1,22 @@
 @if($adsInfo->count())
     @foreach($adsInfo as $p)
-        <div class="ads">
-            <div class="ads-img">
-                <img src="{{User::$img_sirket_src."".$p->logo}}" />
-
-            </div>
-
-            <div class="ads-text">
-                <div class="com_name">
-                    {{ucwords($p->com_name)}}
+        <a href="ads/{{$p->id}}">
+            <div class="ads">
+                <div class="ads-img">
+                    <img src="{{User::$img_sirket_src."".$p->logo}}" />
                 </div>
-                <div class="ads_name">
-                    {{$p->ads_name}}
+
+                <div class="ads-text">
+                    <div class="com_name">
+                        {{ucwords($p->com_name)}}
+                    </div>
+                    <div class="ads_name">
+                        {{$p->ads_name}}
+                    </div>
+                    
                 </div>
-                
             </div>
-        </div>
+        </a>
     @endforeach
 @endif
 
