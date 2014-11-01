5517903ac1df01f32db96a01292b40d98a4b88d4
Erdi <erdi.doqan@gmail.com>
2014-11-01 17:29:12 +0100

    navbar daki bildirim merkezi ve job silindi

--- app/views/partials/nav.blade.php
+++ app/views/partials/nav.blade.php
@@ -6,36 +6,12 @@
 	    </a>
     	
 	   <nav  class="navbar navbar-default navbar-static-top">
-	    	@if(Auth::user())
-	      		<div id="logo-group" style="margin-left:15px;">
-	        		<span id="activity" class="activity-dropdown"> <i class="glyphicon glyphicon-globe"></i> <b class="badge"> 21 </b> </span>
-	        	<div class="ajax-dropdown">
-	        		<div class="btn-group btn-group-justified" data-toggle="buttons">
-	            	<label class="btn btn-default">
-		            	<input type="radio" name="activity" id="ajax/notify/mail.php">
-		              		Messages (14) </label>
-	            	<label class="btn btn-default">
-	              	<input type="radio" name="activity" id="ajax/notify/notifications.html">
-	              		Notification (3) </label>
-  	          </div>
-	          <div class="ajax-notifications custom-scroll">
-	            <div class="alert alert-transparent">
-	              <h4>Click a button to show messages here</h4>
-	              This blank page message helps protect your privacy, or you can show the first message here automatically.
-	            </div>
-	            <i class="fa fa-lock fa-4x fa-border"></i>
-	          </div>
-	        </div>
-	      </div>
-	@endif
+	    	
 	<div class="turgittin">
 		<div class="project-context hidden-xs">
 	        <a class="project-selector" href="/"><i class="fa fa-home"></i></a>
 			
 		</div>
-	      <div class="project-context hidden-xs">
-	        <a class="project-selector" href="search"> Job Ads</a>
-	      </div>
 	       <div class="project-context hidden-xs">
 	        
 	        <a class="project-selector" href="/my-page"> My Page</a>
@@ -48,16 +24,6 @@
 	          <li><a href="/pre-written">My Pre-Written</a></li>
 	          <li><a href="/">My Message</a></li>
 	          <li class="divider"></li><li><a href="/"><i class="fa fa-file-pdf-o"></i> File (PDF)</a></li>
-	        </ul>
-	      </div>
-	      <div class="project-context hidden-xs">
-	        <span class="project-selector dropdown-toggle" data-toggle="dropdown"> Sector <i class="fa fa-angle-down"></i></span>
-	        <ul class="dropdown-menu">
-	          <li><a href="#">Insurance</a></li>
-	          <li><a href="#">Information</a></li>
-	          <li><a href="#">Education</a></li>
-	          <li><a href="#">Finances</a></li>
-	          <li><a href="#">Construction</a></li>
 	        </ul>
 	      </div>
 	      </div>
