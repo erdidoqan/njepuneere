bfc715ec5a17bee8a34f2885f0f82f9d23d085cc
Erdi <erdi.doqan@gmail.com>
2014-11-20 03:12:57 +0100

    404 page not found yapildi

--- app/views/partials/sidebar.blade.php
+++ app/views/partials/sidebar.blade.php
@@ -1,4 +1,41 @@
-<img src="http://104.131.119.56/img/5.PNG" alt="...">
+
+
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="http://104.131.119.56/img/5.PNG">
+      <div class="carousel-caption">
+        Build your career with njepuneere..
+      </div>
+    </div>
+    <div class="item">
+      <img src="http://104.131.119.56/img/5.PNG">
+      <div class="carousel-caption">
+        Doors of a new world..
+      </div>
+    </div>
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+
 
 <div class="panel panel-success" style="margin-top:51px;">
 	<div class="panel-body">
---
+++ app/views/site/404.blade.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    
+@include('partials/header')
+
+
+  </head>
+
+  <body>
+
+      @include('partials/nav')
+        
+         <div class="section1">     
+<div class="container">
+  <div class="col-md-12">
+    
+    <div class="col-sm-7"> 
+                {{ Form::open(array('url' => 'search')) }}
+          <input id="search-fld" class="form-control input-lg"  type="text" name="search" placeholder="Find jobs and more" data-autocomplete='
+          "ActionScript",
+          "AppleScript",
+          "Asp",
+          "BASIC",
+          "C",
+          "C++",
+          "Clojure",
+          "COBOL",
+          "ColdFusion",
+          "Erlang",
+          "Fortran",
+          "Groovy",
+          "Haskell",
+          "Java",
+          "JavaScript",
+          "Lisp",
+          "Perl",
+          "PHP",
+          "Python",
+          "Ruby",
+          "Scala",
+          "Scheme"'>
+          
+       
+        </div>
+    <div class="col-sm-3">
+                
+                {{Form::work_place()}}
+    </div>
+
+    <div class="col-sm-2">
+               <span class="input-group-btn">
+                    <button type="submit" class="btn btn-success btn-block btn-lg" ><span class="glyphicon glyphicon-search"></span> Job Search</button>
+                </span>
+    </div>
+     {{ Form::close() }}
+  </div>
+</div>
+</div>
+
+<div class="container">
+        <div class="col-sm-12" style="margin-top:30px;">
+       <div class="error-template">
+                <h1>
+                    Oops!</h1>
+                <h2>
+                    404 Not Found</h2>
+                <div class="error-details">
+                    Sorry, an error has occured, Requested page not found!
+                </div>
+                <div class="error-actions">
+                    <a href="/" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-home"></span>
+                        Take Me Home </a>
+                        @if(!Auth::user())
+                        <a href="/BireyGiris" class="btn btn-primary btn-lg"><span class="glyphicon glyphicon-user"></span> To Login </a>
+                        <a href="/BireyKayit" class="btn btn-success btn-lg"><span class="glyphicon glyphicon-user"></span> To Register </a>
+                        @endif
+                </div>
+            </div>
+         </div>
+      
+</div>
+@include('partials/footer')
+<script type="text/javascript">
+$(document).ready(function() {
+  $('html').animate({scrollTop: 0},'slow');});
+</script>
+  </body>
+</html>
+
