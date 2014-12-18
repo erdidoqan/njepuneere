0372e21d50923ad054775fff5348468c41e1afa9
Erdi <erdi.doqan@gmail.com>
2014-12-18 23:46:02 +0200

    scrpits

--- app/views/partials/scripts.blade.php
+++ app/views/partials/scripts.blade.php
@@ -18,17 +18,15 @@
 {{HTML::script("/js/jasny-bootstrap.min.js")}}
 {{HTML::script("/js/sekil.js")}}
 
-
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
     <script>
       if (!window.jQuery) {
-        document.write('<script src="/js/libs/jquery-2.0.2.min.js"><\/script>');
+        document.write('<script src="js/libs/jquery-2.0.2.min.js"><\/script>');
       }
     </script>
 
     <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
     <script>
       if (!window.jQuery.ui) {
-        document.write('<script src="/js/libs/jquery-ui-1.10.3.min.js"><\/script>');
+        document.write('<script src="js/libs/jquery-ui-1.10.3.min.js"><\/script>');
       }
     </script>
