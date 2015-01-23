b56394e25ecc3c64c12c267393ce04a5abec7e49
Erdi <erdi.doqan@gmail.com>
2015-01-23 17:45:26 +0100

    footer guzellesti

--- app/views/partials/footer.blade.php
+++ app/views/partials/footer.blade.php
@@ -25,6 +25,9 @@
                         <li> <a href="http://njeboteere.org/web/" target="_blank"> Nje Bote e Re </a> </li>
                         <li> <a href="http://njeboteere.org/web/misioni/" target="_blank"> Vision </a> </li>
                         <li> <a href="http://njeboteere.org/web/category/ndermjetsim/" target="_blank"> Mentorship </a> </li>
+                        <li> <a href="/aboutus"> About Us </a> </li>
+                        <li> <a href="http://njeboteere.org/web/" target="_blank"> Nje Bote e Re </a> </li>
+                        <li> <a href="http://njeboteere.org/web/misioni/" target="_blank"> Vision </a> </li>
                     </ul>
                 </div>
                 <div class="col-lg-2">
@@ -34,6 +37,9 @@
                         <li> <a href="/privacy"> Privacy Principles </a> </li>
                         <li> <a href="/term-of-membership"> Terms of Membership </a> </li>
                         <li> <a href="/use-of-content"> Use of Content </a> </li>
+                        <li> <a href="/privacy"> Privacy </a> </li>
+                        <li> <a href="/privacy"> Privacy Principles </a> </li>
+                        <li> <a href="/term-of-membership"> Terms of Membership </a> </li>
                     </ul>
                 </div>
                 <div class="col-lg-2">
@@ -43,6 +49,10 @@
                         <li> <a href="/term-of-use"> Responsibilities </a> </li>
                         <li> <a href="/use-of-content"> Use of Content </a> </li>
                         <li> <a href="/use-of-content"> Use of Cookies </a> </li>
+                        <li> <a href="/term-of-use"> Terms of Use </a> </li>
+                        <li> <a href="/term-of-use"> Responsibilities </a> </li>
+                        <li> <a href="/use-of-content"> Use of Content </a> </li>
+
                     </ul>
                 </div>
                 <div class="col-lg-2">
@@ -50,24 +60,35 @@
                     <ul>
                         <li> <a href="#"> Advertise </a> </li>
                         <li> <a href="#"> Web Advertise </a> </li>
-                        <li> <a href="#"> Alumni Directory </a> </li>
+                        <li> <a href="#"> Alumni Directory </a> </li><li> <a href="#"> Advertise </a> </li>
+                        <li> <a href="#"> Web Advertise </a> </li>
+                        <li> <a href="#"> Alumni Directory </a> </li><li> <a href="#"> Advertise </a> </li>
                     </ul>
                 </div>
-                <div class="col-lg-2">
-                    <h3> Advertise </h3>
-                    <ul>
-                        <li> <a href="#"> Advertise </a> </li>
-                        <li> <a href="#"> Web Advertise </a> </li>
-                        <li> <a href="#"> Alumni Directory </a> </li>
-                    </ul>
-                </div>
-                <div class="col-lg-2">
-                    <h3> Advertise </h3>
-                    <ul>
-                        <li> <a href="#"> Advertise </a> </li>
-                        <li> <a href="#"> Web Advertise </a> </li>
-                        <li> <a href="#"> Alumni Directory </a> </li>
-                    </ul>
+                <div class="col-lg-4">
+                    <h3> Contact Us </h3>
+                    <form role="form" method="post" id="iletisim">
+                    
+                    <div class="col-sm-6">
+                        <div class="form-group" style="margin-bottom:5px;">
+                            <input type="text" id="isim" name="isim" class="form-control" placeholder="İsim Soyisim">
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="femail" name="femail" placeholder="email adresiniz">
+                        </div>
+                    </div>
+                    <div class="col-sm-12">
+                        <div class="form-group">
+                            <textarea name="mesaj" id="mesaj" class="form-control" rows="3" required="" placeholder="Bize iletmek istediğiniz mesajınız..."></textarea>
+                        </div>
+                    </div>
+                    <div class="col-sm-12">
+                        <button type="submit" class="btn btn-default btn-block">Mesajı İlet</button>
+                    </div>
+                        
+                    </form>
                 </div>
             </div>
             <!--/.row--> 
@@ -75,17 +96,18 @@
         <!--/.container--> 
     </div>
     <!--/.footer-->
-    
+    <div class="container">
+            <div class="cizgi">
+            </div>
+    </div>
     <div class="footer-bottom">
         <div class="container">
-        <div class="col-sm-12">
             <p class="pull-center text-muted"> Klienti nuk duhet t'i tregojë një personi të tretë informacionet e antarësimit të instuticionit, kodin, të dhënat personale të kandidatit dhe informacionet private (të veçanta). Të marrë masa, nëse është e nevojshme, për ruajtien e informacionit dhe mosdhënien e këtyre informacioneve një personi të tretë.
                 Si dhe të marrë përsipër ruajtien e tyre.
                 Klienti, perveç kësaj, në rast të ndryshimit të antarit përgjegjës që përdorte kodin e antarësimit të instuticionit, duhet të informojë menjëherë njepuneere.com.
                 Klienti nëse është kunder me instuticionin, prish menjëherë kontratën me <a href="http://njepuneere.com/" class="text-muted"><strong>Njepuneere.com</strong> </a>.
                 Gjithashtu mund të bëjë një kërkesë për t'iu shpaguar çdo lloj dëmi të bërë kundrejt tij. </p><br>
             <p class="pull-center text-muted"> Copyright © njepuneere.com 2014. All right reserved.  </p>
-        </div>
         </div>
     </div>
     <!--/.footer-bottom--> 
