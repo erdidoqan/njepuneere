b3b81b389c7e86e0c66942945a80ab9aec77b6db
Erdi <erdi.doqan@gmail.com>
2015-01-29 22:29:02 +0100

    nav big button

--- app/views/partials/nav.blade.php
+++ app/views/partials/nav.blade.php
@@ -22,7 +22,7 @@
 		<div class="pull-right">
 			@if(!Auth::user())
 			<a style="margin-top:8px;" data-toggle="modal" href="#myModal" class="btn btn-primary pull-right header-btn hidden-mobile"><i class="fa fa-circle-arrow-up fa-lg"></i> Login & Register</a>
-			<a style="margin-bottom:-24px;margin-right:5px;" href="{{User::$img_sirket_src}}" class="btn btn-success header-btn"><i class="fa fa-circle-arrow-up fa-lg"></i> Employer</a>
+			<a style="margin-bottom:-21px;margin-right:5px;" href="{{User::$img_sirket_src}}" class="btn btn-success header-btn"><i class="fa fa-circle-arrow-up fa-lg"></i> Employer</a>
 			@endif
 
 			@if(Auth::user())
