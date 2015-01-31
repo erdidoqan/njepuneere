9de513f1b8222b03f5b5d3a5f7b519248776a4cf
Erdi <erdi.doqan@gmail.com>
2015-01-31 16:39:22 +0100

    header

--- app/views/partials/header.blade.php
+++ app/views/partials/header.blade.php
@@ -1,5 +1,7 @@
 <meta charset="utf-8">
 <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0">-->
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="description" content="">
 <meta name="author" content="Erdi Doğan">
 <link rel="shortcut icon" href="/ana.ico" type="image/x-icon">
