cd105c4c27600aaf71fc608bc17215e40e28773f
Erdi <erdi.doqan@gmail.com>
2015-01-31 05:02:50 +0100

    search halledildi.

--- app/models/Ara.php
+++ app/models/Ara.php
@@ -15,19 +15,13 @@ class Ara extends \Eloquent
         'columns' => [
             'ads_name' => 10,
             'com_name' => 10,
-            'work_place' => 10,
+            'work_place' => 2,
             'languages' => 5,
-            'job_desc' =>50,
-            'qua' =>50,
-        ],
-        'joins' => [
-            'Ads' => ['ads_infos.user_id','Ads.user_id'],
+            'job_desc' => 50,
+            'qua' => 50,
         ],
     ];
 
-    public function posts()
-    {
-        return $this->hasMany('Ads');
-    }
+    
 
 }
