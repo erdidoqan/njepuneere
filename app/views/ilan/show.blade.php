ae19991a26c1d52017bed043724f068cfeb27be6
Erdi <erdi.doqan@gmail.com>
2015-02-03 18:09:28 +0100

    share

--- app/views/ilan/show.blade.php
+++ app/views/ilan/show.blade.php
@@ -161,8 +161,7 @@
 
               <ul class="dropdown-menu">
                 <li>
-                  <a href="https://www.facebook.com/sharer/sharer.php?u=http://njepuneere.com/ads/39" class="fa fa-facebook" target="_blank"> Facebook</a>
-                <div class="fb-share-button" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button_count"></div>
+                <a class="fa fa-facebook" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button_count"></a>
                 </li>
                 <li>
                   <a href="#" class="fa fa-twitter"> Twitter</a>
