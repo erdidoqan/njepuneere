6cad86ee24f580ff9dc35e579300f409f272445f
Erdi <erdi.doqan@gmail.com>
2015-02-04 01:56:44 +0100

    share

--- app/views/partials/header.blade.php
+++ app/views/partials/header.blade.php
@@ -2,7 +2,6 @@
 <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0">-->
 
 <meta name="description" content="">
-<meta name="author" content="Erdi Doğan">
 <link rel="shortcut icon" href="/ana.ico" type="image/x-icon">
 <link rel="icon" href="/ana.ico" type="image/x-icon">
 <meta name="google-site-verification" content="HpQgEQ1gHXC_DrfCXZTHSFVXZMkVxTLESkJuMz2hq8A" />
