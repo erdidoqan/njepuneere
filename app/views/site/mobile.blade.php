6fdd25360285470bff56616c8b65a290ebce02ff
Erdi <erdi.doqan@gmail.com>
2015-02-12 05:12:51 +0100

    nav bar

--- app/views/site/mobile.blade.php
+++ app/views/site/mobile.blade.php
@@ -19,34 +19,8 @@
 <body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
 <nav class="navbar navbar-default navbar-fixed-top" id="nav">
 <div class="container">
-    <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-        </button>
+    <div
         <a class="navbar-brand" href="/"><img src="/img/logo.png" class="img-responsive" style="width:80%;"></a>
-    </div>
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        <ul class="nav navbar-nav">
-            <li>
-                <a href="neler-yapiyoruz">NELER YAPIYORUZ?</a>
-            </li>
-            <li>
-                <a href="projeler">PROJELER</a>
-            </li>
-            <li>
-                <a href="sosyal-duyarlilik">SOSYAL DUYARLILIK</a>
-            </li>
-            <li>
-                <a href="#">BLOG</a>
-            </li>
-            <li>
-                <a href="iletisim">İLETİŞİM</a>
-            </li>
-
-        </ul>
     </div>
     <!-- /.navbar-collapse -->
 </div>
