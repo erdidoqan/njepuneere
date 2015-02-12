f6203095460d5fb5e2670d102a74fc21ad58ee8f
Erdi <erdi.doqan@gmail.com>
2015-02-12 04:46:26 +0100

    mobil

--- app/views/site/mobile.blade.php
+++ app/views/site/mobile.blade.php
@@ -7,23 +7,16 @@
 
 @section('content')
 
-<nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="/"> <img src="/img/logo.png"> </a>
+
+<div class="container">
+  
+
+  <div class="row"> 
+    <div class="col-sm-12 col-xs-12">
+      <img src="/img/logo.png">
+      
     </div>
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    
-  </div><!-- /.container-fluid -->
-</nav>
-
+  </div>
+</div>
 
 @stop
